96fc0c184904b88b9ce2c08ac3a387b71619c4a9
lokendra dhote <lokendradhote0@gmail.com>
2026-03-15 19:08:11 +0530

    Fixed Jenkins gpg error

--- devopstools.sh
+++ devopstools.sh
@@ -62,11 +62,12 @@ sudo apt install -y trivy
 # --- JENKINS INSTALLATION ---
 echo "--- Installing Jenkins (OpenJDK 21) ---"
 sudo apt install -y fontconfig openjdk-21-jre
-
-sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-
-echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | \
-sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+ 
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 sudo apt update -y
 sudo apt install -y jenkins
